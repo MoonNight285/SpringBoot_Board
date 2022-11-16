ed87978150ce84fc11be9a7e5520885c6ae6a202
MoonNight285 <kimjyjh11@naver.com>
2022-11-16 09:59:10 +0900

    파일 업로드를 하기 위한 설정정보를 스피링부트에 적용

--- src/main/java/com/bitc/board/Board1Application.java
+++ src/main/java/com/bitc/board/Board1Application.java
@@ -2,8 +2,11 @@ package com.bitc.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication => 가장 기본으로 실행되는 옵션
+// exclude 옵션을 사용해서 MultipartAutoConfiguration 클래스의 자동 구성을 사용하지 않도록 설정
+@ SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class Board1Application {
     
     public static void main(String[] args) {
